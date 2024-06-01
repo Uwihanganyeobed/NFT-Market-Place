a72fdf1ce1e2f9dd70ebd0403af3662610149a01
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-06-01 17:54:07 +0200

    reusable1

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 // App.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,Button } from 'react-native';
 import tw from '../app/talwind'; // adjust the path if necessary
 
 export default function App() {
   return (
-    <View style={tw`flex-1 justify-center items-center bg-blue-500`}>
-      <Text style={tw`text-white text-lg`}>Hello, Tailwind!</Text>
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text style={tw`text-lg`}>Hello, Tailwind!</Text>
+      <Button style={tw`text-white text-sm`}
+        title='Click Me'
+        onPress={()=>alert('Hello, Tailwind')}
+        />
     </View>
   );
 }
